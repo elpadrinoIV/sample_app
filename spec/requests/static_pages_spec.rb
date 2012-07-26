c75502832064998311d1b91cd48527318721022d
Nicolas Dascanio <nd@nd.com>
2012-07-26 16:38:36 -0300

    no tengo idea q cambie

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "RoR | Home")
+                        :text => "RoR")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
